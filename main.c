1e5cf70ac0e5b426b5f68eab82b1ef168a22ae41
Edsonfrs <edsonfrs@gmail.com>
2024-03-20 13:36:57 -0300

    feat: Initial project setup: This commit initializes the project with basic directory structure and essential configuration files

---
+++ main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definindo uma constante para a quantidade máxima de alunos aceitos no sistema
+#define MAX_ALUNOS 50
+
+// Definindo a struct que armazenará os dados dos alunos
+typedef struct {
+    char nome[50];
+    float notas[4];
+    int ativo;
+} Aluno;
+
+// Array para armazenamento de notas e nomes dos alunos
+Aluno alunos[MAX_ALUNOS];
+
+// definindo os protótipos do Menu e dos "métodos" do Menu
+void menu();
+void cadastrar();
+void remover();
+void reprovados();
+void pesquisar();
+void listar();
+
+// Método main
+int main() {
+    menu();
+    return 0;
+}
+
+// Desenvolvendo o Menu do sistema
+void menu() {
+
+
+    int op;
+    //printf("\nSistema De Controle Escolar - SCE\n");
+    do {
+        system("clear"); // For Linux
+        //system("cls"); // For Windows
+        printf("\n1 - Cadastrar Aluno \n2 - Remover Aluno");
+        printf("\n3 - Alunos Reprovados \n4 - Pesquisar Aluno");
+        printf("\n5 - Listar Alunos \n\n0 - Sair \n");
+        scanf("%d", &op);
+        getchar();
+
+        switch (op)
+        {
+            case 1:
+                cadastrar();
+                break;
+            case 5:
+                listar();
+                break;
+
+        }
+        //getchar();
+    } while (op != 0);
+}
+
+// Métodos Menu
+void cadastrar() {
+    char nome[50];
+    float notas[4];
+
+    int op;
+    do {
+        system("clear"); // For Linux
+        //system("cls"); // For Windows
+        printf("\nNome: ");
+        fgets(nome, sizeof(nome), stdin);
+        printf("\n1º Bimestre: ");
+        scanf("%f", &notas[0]);
+        printf("\n2º Bimestre: ");
+        scanf("%f", &notas[1]);
+        printf("\n3º Bimestre: ");
+        scanf("%f", &notas[2]);
+        printf("\n4º Bimestre: ");
+        scanf("%f", &notas[3]);
+
+
+        for (int i = 0; i < MAX_ALUNOS; ++i)
+        {
+            if ( alunos[i].ativo == 0 )
+            {
+                alunos[i].notas[0] = notas[0];
+                alunos[i].notas[1] = notas[1];
+                alunos[i].notas[2] = notas[2];
+                alunos[i].notas[3] = notas[3];
+                strcpy(alunos[i].nome, nome);
+                alunos[i].ativo = 1;
+                break;
+            }
+
+        }
+
+        printf("\n1 - Continuar \n0 - Sair \n");
+        scanf("%d", &op);
+        getchar();
+    } while (op != 0);
+}
+
+void remover() {
+
+}
+
+void reprovados() {
+
+}
+
+void pesquisar() {
+
+}
+
+void listar() {
+    system("clear"); // For Linux
+    //system("cls"); // For Windows
+    printf("\nLISTA DE ALUNOS\n");
+    for (int i = 0; i < MAX_ALUNOS; ++i)
+    {
+        if (alunos[i].ativo == 1)
+        {
+            printf("Nome: %s\n", alunos[i].nome);
+            printf("1º Bimestre %0.2f\n", alunos[i].notas[0]);
+            printf("2º Bimestre %0.2f\n", alunos[i].notas[1]);
+            printf("3º Bimestre %0.2f\n", alunos[i].notas[2]);
+            printf("4º Bimestre %0.2f\n", alunos[i].notas[3]);
+            printf("\n---------------------------------------\n");
+
+        }
+    }
+    getchar();
+}
